b7e10f9b564ea9c78dbbd10a81e24536c9aceea9
Nikita Gilevski <gilevskini2004@gmail.com>
2024-06-27 02:48:51 +0200

    debounce

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,19 +2,16 @@ import BookList from "../../features/Booklist";
 import Filters from "../../features/Filters";
 import Header from "../../features/Header";
 import styles from "./home.module.scss";
-import { FilterProvider } from "../../entites/useFilter";
 
 const Home = () => {
   return (
-    <FilterProvider>
-      <div className={styles.container}>
-        <Header />
-        <div className={styles.content}>
-          <Filters />
-          <BookList />
-        </div>
+    <div className={styles.container}>
+      <Header />
+      <div className={styles.content}>
+        <Filters />
+        <BookList />
       </div>
-    </FilterProvider>
+    </div>
   );
 };
 
